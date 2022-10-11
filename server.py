c68716c3a5fa2942be0b823c4547490e35a6a8bf
“Julia <juliaabkhan@gmail.com>
2022-10-11 11:53:00 -0500

    Completed main assignment.
    Co-Authored-By: leyasalazar 96083374+leyasalazar@users.noreply.github.com

--- server.py
+++ server.py
@@ -13,18 +13,26 @@ AWESOMENESS = [
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
     'wonderful', 'smashing', 'lovely']
 
+INSULTS = [
+  'stupid', 'ugly', 'mean', 'slow'
+]
 
 @app.route('/')
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+      <!doctype html>
+        <html>
+          <p>Hi! This is the home page.</p>
+          <a href="/hello">Hello</a>
+        </html>"""
 
 
 @app.route('/hello')
 def say_hello():
     """Say hello and prompt for user's name."""
-
+      
     return """
     <!doctype html>
     <html>
@@ -33,8 +41,30 @@ def say_hello():
       </head>
       <body>
         <h1>Hi There!</h1>
+        <form action="/diss">
+          What's your name? <input type="text" name="person">
+          <input type="submit" value="Submit">
+        </form>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          <div>
+                <label for="compliment_choice">Choose your compliment:</label>
+                <select name="compliment" id="compliment_choice">
+                    <option value="awesome">awesome</option>
+                    <option value="terrific">terrific</option>
+                    <option value="neato">neato</option>
+                    <option value="fantabulous">fantabulous</option>
+                    <option value="wowza">wowza</option>
+                    <option value="oh-so-not-meh">oh-so-not-meh</option>
+                    <option value="brilliant">brilliant</option>
+                    <option value="ducky">ducky</option>
+                    <option value="coolio">coolio</option>
+                    <option value="incredible">incredible</option>
+                    <option value="wonderful">wonderful</option>
+                    <option value="smashing">smashing</option>
+                    <option value="lovely">lovely</option>
+                </select>
+            </div>
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -48,7 +78,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
     return f"""
     <!doctype html>
@@ -61,7 +91,25 @@ def greet_person():
       </body>
     </html>
     """
+@app.route('/diss')    
+def diss_person():
+    """Get user by name."""
 
+    player = request.args.get("person")
+
+    insult = choice(INSULTS)
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {insult}!
+      </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
